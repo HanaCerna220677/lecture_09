b33ae1ca864ee0d93da44615a10fd9383115739a
Hana Cerna <220677@vutbr.cz>
2022-04-12 09:16:24 +0200

    function

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,11 +13,19 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path) as data_file:
+        data = json.load(data_file)
+    return data[field]
+
+    if field in data.keys():
+        return data[field]
+    else:
+        return None
 
 
 def main():
-    pass
-
+    unordered_numbers = read_data("sequential.json", "unordered_numbers")
+    print(unordered_numbers)
 
 if __name__ == '__main__':
     main()
